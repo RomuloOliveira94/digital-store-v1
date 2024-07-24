4c894fd096d68d7b8abb2d6702443ad9ae89cf6b
Rômulo Augusto <romuloffall@gmail.com>
2024-07-24 12:02:49 -0300

    #implement layout and initial components

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,24 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+    },
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        storelight: {
+          primary: "#C92071",
+          secondary: "#B5B6F2",
+          accent: "#991956",
+          error: "#EE4266",
+          success: "#52CA76",
+          warning: "#F6AA1C",
+        },
+      },
+      "dark",
+    ],
+  },
 };
